ab1f1570910d6894f25173e304a83c58280b92d6
Daniel Vijayakumar <daniel.vkumar@gmail.com>
2017-05-02 01:46:19 -0400

    Almost completed the Register form. YES.

---
+++ ui/src/app/helper.ts
@@ -0,0 +1,22 @@
+/// Provinces of Canada ///
+
+export class CanadianProvince {
+   name: string
+   shortname: string
+};
+
+export const CANADIANPROVINCES: CanadianProvince[] = [
+   {name: "Ontario", shortname: "ON"},
+   {name: "Alberta", shortname: "AB"},
+   {name: "British Columbia", shortname: "BC"},
+   {name: "Manitoba", shortname: "MB"},
+   {name: "New Brunswick", shortname: "NB"},
+   {name: "Newfoundland and Labrador", shortname: "NL"},
+   {name: "Northwest Territories", shortname: "NT"},
+   {name: "Nova Scotia", shortname: "NS"},
+   {name: "Nunavut", shortname: "NU"},
+   {name: "Prince Edward Island", shortname: "PE"},
+   {name: "Quebec", shortname: "QC"},
+   {name: "Saskatchewan", shortname: "SK"},
+   {name: "Yukon", shortname: "YT"}
+];
--- ui/src/app/register/register.component.ts
+++ ui/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { CanadianProvince, CANADIANPROVINCES } from '../helper';
 
 @Component({
   selector: 'app-register',
@@ -10,6 +12,23 @@ export class RegisterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // We assume that jQuery is loaded in this project
+    $('.datepicker').pickadate({
+       selectMonths: true, // Creates a dropdown to control month
+       selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
   }
 
+  provinces: CanadianProvince[] = CANADIANPROVINCES;
+
+  onClickSubmit (): void {
+    // TODO: Present review/confirmation screen
+    alert("TODO: Submitted!");
+  };
+
+  @Input() canPhotoChild: boolean = false;
+
+  onChangeCanPhotoChild (event): void {
+    this.canPhotoChild = event.target.checked;
+  }
 }
